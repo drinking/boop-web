dcc1a39b82a64ac4ef30bb8f80c4623a3f9dfe83
panjunqing <panjunqing@ke.com>
2022-01-23 19:59:35 +0800

    update

--- src/lib/list.js
+++ src/lib/list.js
@@ -18,11 +18,24 @@ var METHODS = {
     },
     usage: "Sort number list "
   },
-  'ReverseList':{
-    func:function (text) {
+  'ReverseList': {
+    func: function(text) {
       return text.replace(/\n$/, '').split('\n').reverse().join('\n');
     },
     usage: "Reverse element of list"
+  },
+  'UniqueList': {
+    func: function(text) {
+      var set = new Set(text.replace(/\n$/, '').split('\n'))
+      return Array.from(set).join('\n');
+    },
+    usage: "Make elements in list is unique"
+  },
+  'CountList': {
+    func: function(text) {
+      return text.replace(/\n$/, '').split('\n').length
+    },
+    usage: "Count line number of a list"
   }
 }
 
